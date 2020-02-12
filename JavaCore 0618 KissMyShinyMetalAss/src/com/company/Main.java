2d97d0d02ca2228530211de409a7f344f73dc222
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-12 09:54:48 +0200

    Add files via upload

---
+++ JavaCore 0618 KissMyShinyMetalAss/src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+
+0618 KissMyShinyMetalAss
+
+Создай класс с именем KissMyShinyMetalAss.
+Создай объект этого класса, выведи его на экран.
+Требования:
+1. Создай класс KissMyShinyMetalAss внутри класса Solution.
+2. Класс KissMyShinyMetalAss должен быть статическим.
+3. Класс KissMyShinyMetalAss должен быть public.
+4. В методе main создай объект класса KissMyShinyMetalAss.
+5. В методе main выведи объект класса KissMyShinyMetalAss на экран.
+
+*/
+
+// public class Solution
+public class Main {
+
+    public static class KissMyShinyMetalAss {}
+    public static void main(String[] args) {
+
+        System.out.println(new KissMyShinyMetalAss());
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
